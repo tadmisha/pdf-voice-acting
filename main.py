d1ee435b5cf96cd79aa02fabb6479a23b6960911
tadmisha <tadmisha@gmail.com>
2022-08-20 16:05:31 +0400

    Check pdf path is ok

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from gtts import gTTS
 import pathlib
 
 def read_pdf(pdf_path):
-    pass
-
+    print('start reading pdf...')
+    if not pathlib.Path(pdf_path).exists():
+        return "This path is empty"
+    else:
+        if not pdf_path.endswith('.pdf'):
+            return "This file is not pdf"
 
 def text_to_speech(speech_filename, text, lang):
     pass
 
 def main():
-    pass
+    path = input('path to pdf file: ')
+    text = read_pdf(path)
 
 if __name__ == '__main__':
     main()
