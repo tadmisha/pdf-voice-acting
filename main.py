43ad47639fb1d2bfc9589f60a42a559986cba907
tadmisha <tadmisha@gmail.com>
2022-08-20 16:04:47 +0400

    Start

---
+++ main.py
@@ -0,0 +1,16 @@
+from PyPDF2 import PdfReader
+from gtts import gTTS
+import pathlib
+
+def read_pdf(pdf_path):
+    pass
+
+
+def text_to_speech(speech_filename, text, lang):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
